370f6ab3326fa32b0b397bd5fd749910ad612f36
ArturVargas <arturvargas9226@gmail.com>
2019-07-08 00:25:26 -0500

    deploy

---
+++ server.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/blog'));
+
+app.get('*', function(req,res) {
+  // Replace the '/dist/<to_your_project_name>/index.html'
+  res.sendFile(path.join(__dirname + '/dist/blog/index.html'));
+});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
